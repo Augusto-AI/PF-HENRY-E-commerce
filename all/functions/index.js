7e1b15c65b64f6f9b33de97fae945f1c27a9c781
Danylopgali <daniellopg@hotmail.com>
2023-09-27 14:34:34 -0600

    send mail

--- all/functions/index.js
+++ all/functions/index.js
@@ -1,7 +1,14 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
+
+const sgMail = require('@sendgrid/mail');
+
+
+
 
 admin.initializeApp();
+
 
 exports.lowercaseProductName = functions.firestore.document('/products/{documentId}')
     .onCreate((snap, context) => {
@@ -12,4 +19,27 @@ exports.lowercaseProductName = functions.firestore.document('/products/{document
         const lowercaseName = name.toLowerCase();
 
         return snap.ref.update({ name_lower: lowercaseName });
+    });
+
+
+
+    sgMail.setApiKey('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');
+
+    exports.sendConfirmationEmail = functions.https.onCall(async (data, context) => {
+      const { userEmail } = data;
+    
+      const msg = {
+        to: userEmail,
+        from:  'startvideogame11@gmail.com',
+        subject: "Confirmación de Pago Exitoso",
+        text: "Tu pago ha sido procesado con éxito. Gracias por tu compra.",
+      };
+    
+      try {
+        await sgMail.send(msg);
+        return { success: true };
+      } catch (error) {
+        console.error("Error al enviar el correo electrónico de confirmación:", error);
+        return { success: false, error: error.message };
+      }
     });
